4a832ffc19db745ce7695c5849b955eb6908eb58
jnicklr <jnichollaslr@gmail.com>
2024-01-05 21:40:01 -0300

    feat: database class

--- src/database.py
+++ src/database.py
@@ -1,1 +1,12 @@
 import sqlite3
+
+class DataBase():
+    def __init__(self):
+        self.createTable()
+
+    def createTable(self):
+        connectionDB = sqlite3.connect("database.db")
+        cursorDB = connectionDB.cursor()
+        cursorDB.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
+        connectionDB.commit()
+        connectionDB.close()
